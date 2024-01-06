eb3ec48dc83344c2d327d7ee6cd54a8d35a2e235
llLAWLAWll <lucasgs2218@gmail.com>
2024-01-06 16:59:45 -0300

    Mudanças simples para o GIT

--- projetogit/lib/main.dart
+++ projetogit/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Atividade Git',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Pagina Inicial'),
     );
   }
 }
@@ -106,11 +106,12 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'Quantidade de vezes que o botão foi precionado:',
+              style: TextStyle(color:  Colors.deepPurpleAccent, fontSize: 30),
             ),
             Text(
               '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              style: TextStyle(color: Colors.greenAccent, fontSize: 130),
             ),
           ],
         ),
@@ -118,7 +119,7 @@ class _MyHomePageState extends State<MyHomePage> {
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        child: const Icon(Icons.add_outlined),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
